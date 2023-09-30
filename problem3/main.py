300b49ddbef08bed2d4d44a5759fd31de1e77912
arifin1104 <miftahjuanda21@gmail.com>
2023-09-30 23:05:24 +0700

    clearbasicpart1

--- problem3/main.py
+++ problem3/main.py
@@ -2,5 +2,6 @@
 T = 20.0
 r = 4.0
 
-
 # kode disini
+luas = 2 * 3.14 * r * (r + T)
+print(luas)
--- problem4/main.py
+++ problem4/main.py
@@ -3,3 +3,9 @@ Input Harga: 370000
 Input Diskon: 15
 Output: harga yang harus dibayar adalah Rp. 314.500
 '''
+harga_awal = 370000
+diskon = 15
+total_diskon = (diskon / 100) * harga_awal
+harga_setelah_diskon = harga_awal - total_diskon
+
+print("Harga yang harus dibayar adalah Rp.",harga_setelah_diskon)
